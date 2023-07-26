3d9dc50cf23f825a57bb09121ee3dccd34b18a22
Viet Nguyen <viet34tqc@gmail.com>
2023-07-26 19:04:35 +0700

    Add dropdown to usermenu

--- components/SiteHeader/UserMenu.tsx
+++ components/SiteHeader/UserMenu.tsx
@@ -8,11 +8,16 @@ TODO
 import BarIcon from '@/icons/BarIcon';
 import UserCircleIcon from '@/icons/UserCircleIcon';
 import { useModalStoreActions } from '@/store/useModalStore';
-import { useReducer } from 'react';
+import { useState } from 'react';
+import {
+  CustomDropdown,
+  CustomDropdownContent,
+  CustomDropdownTrigger,
+} from '../ui/DropDown';
 import MenuItem from './MenuItem';
 
 const UserMenu = () => {
-  const [isOpen, toggle] = useReducer(state => !state, false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const { setModalView, setIsOpen } = useModalStoreActions();
 
   return (
@@ -35,10 +40,9 @@ const UserMenu = () => {
         >
           Airbnb your home
         </button>
-
-        <div
-          onClick={toggle}
-          className="
+        <CustomDropdown open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+          <CustomDropdownTrigger
+            className="
           p-4
           md:py-1
           md:px-2
@@ -53,45 +57,44 @@ const UserMenu = () => {
           hover:shadow-md
           transition
           "
-        >
-          <BarIcon />
-          <div className="hidden md:block text-gray-400">
-            <UserCircleIcon fill="currentColor" />
-          </div>
-        </div>
-      </div>
-
-      {isOpen && (
-        <div
-          className="
-            absolute
+          >
+            <BarIcon />
+            <div className="hidden md:block text-gray-400">
+              <UserCircleIcon fill="currentColor" />
+            </div>
+          </CustomDropdownTrigger>
+          <CustomDropdownContent
+            align="end"
+            className="
             rounded-xl
             shadow-md
             w-[40vw]
-            md:w-3/4
+            md:w-[200px]
+            md:right-8
             bg-white
             overflow-hidden
-            right-0
-            top-12
             text-sm
           "
-        >
-          <div className="flex flex-col cursor-pointer">
-            <MenuItem
-              label="Login"
-              onClick={() => {
-                setIsOpen(), setModalView('LOGIN');
-              }}
-            />
-            <MenuItem
-              label="Sign up"
-              onClick={() => {
-                setIsOpen(), setModalView('REGISTER');
-              }}
-            />
-          </div>
-        </div>
-      )}
+          >
+            <div className="flex flex-col cursor-pointer">
+              <MenuItem
+                label="Login"
+                onClick={() => {
+                  setIsDropdownOpen(false)
+                  setIsOpen(), setModalView('LOGIN');
+                }}
+              />
+              <MenuItem
+                label="Sign up"
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                  setIsOpen(), setModalView('REGISTER');
+                }}
+              />
+            </div>
+          </CustomDropdownContent>
+        </CustomDropdown>
+      </div>
     </div>
   );
 };
---
+++ components/ui/DropDown.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const CustomDropdown = DropdownMenuPrimitive.Root;
+
+const CustomDropdownTrigger = DropdownMenuPrimitive.Trigger;
+
+const CustomDropdownContent = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <DropdownMenuPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={cn(
+      'z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+      className
+    )}
+    {...props}
+  />
+));
+CustomDropdownContent.displayName = DropdownMenuPrimitive.Content.displayName;
+
+const CustomDropdownItem = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
+    inset?: boolean;
+  }
+>(({ className, inset, ...props }, ref) => (
+  <DropdownMenuPrimitive.Item
+    ref={ref}
+    className={cn(
+      'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      inset && 'pl-8',
+      className
+    )}
+    {...props}
+  />
+));
+CustomDropdownItem.displayName = DropdownMenuPrimitive.Item.displayName;
+
+const CustomDropdownLabel = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Label>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
+    inset?: boolean;
+  }
+>(({ className, inset, ...props }, ref) => (
+  <DropdownMenuPrimitive.Label
+    ref={ref}
+    className={cn(
+      'px-2 py-1.5 text-sm font-semibold',
+      inset && 'pl-8',
+      className
+    )}
+    {...props}
+  />
+));
+CustomDropdownLabel.displayName = DropdownMenuPrimitive.Label.displayName;
+
+export {
+  CustomDropdown,
+  CustomDropdownContent,
+  CustomDropdownItem,
+  CustomDropdownLabel,
+  CustomDropdownTrigger,
+};
