3222f44f9314cd0071aab629f32f9b8c28ec8bff
viet34tqc <viet34tqc@gmail.com>
2023-09-01 22:50:44 +0700

    Categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Categories from '@/components/home/Categories/Categories';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
+    <>
+      <Categories />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    </>
   );
 }
---
+++ components/home/Categories/Categories.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+
+import CategoryBox from './CategoryBox';
+import { categories } from './constants';
+
+const Categories = () => {
+  return (
+    <div className="container">
+      <div
+        className="
+          pt-4
+          flex
+          flex-row
+          items-center
+          justify-between
+          overflow-x-auto
+        "
+      >
+        {categories.map(item => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
