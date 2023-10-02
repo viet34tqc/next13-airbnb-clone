c858710ff08206a340e54c2c7e9fb70281551fbb
viet34tqc <viet34tqc@gmail.com>
2023-10-02 14:29:52 +0700

    Rearrange folder

--- modules/HomePage/components/Listings/FavoriteButton.tsx
+++ modules/HomePage/components/Listings/FavoriteButton.tsx
@@ -3,7 +3,7 @@
 import { cn } from '@/lib/utils';
 import { HeartIcon } from '@heroicons/react/24/outline';
 import { User } from '@prisma/client';
-import useFavorite from './hooks/useFavorite';
+import useFavorite from '../../hooks/useFavorite';
 
 // This component use a hook (client component) => it must be client component
 // => we cannot call `getCurrentUser` here
