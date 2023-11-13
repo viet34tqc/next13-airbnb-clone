d220e7c55e4120bcfbe24cc36bf4fb99dc147ce1
viet34tqc <viet34tqc@gmail.com>
2023-11-13 17:29:13 +0700

    Delete listing with server action

--- app/properties/actions.ts
+++ app/properties/actions.ts
@@ -8,25 +8,21 @@ import getCurrentUser from '../actions/getCurrentUser';
 type State = {
   message?: string;
 };
-
-export async function cancelReservation(
-  reservationId: string,
-  prevState: State
-) {
+export async function deleteListing(listingId: string, prevState: State) {
   try {
     const currentUser = await getCurrentUser();
     if (!currentUser) {
       throw new Error(USER_NOT_FOUND_MESSAGE);
     }
 
-    await db.reservation.deleteMany({
+    await db.listing.delete({
       where: {
-        id: reservationId,
+        id: listingId,
         userId: currentUser.id,
       },
     });
-    revalidatePath('/trips');
-    return { message: 'Cancel reservation successfully' };
+    revalidatePath('/properties');
+    return { message: 'Delete listing successfully' };
   } catch (error) {
     return {
       message: error instanceof Error ? error.message : 'Database error',
--- app/properties/components/DeleteListingButton.tsx
+++ app/properties/components/DeleteListingButton.tsx
@@ -1,45 +1,32 @@
 'use client';
 
 import Button from '@/components/ui/Button';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import toast from 'react-hot-toast';
+import { useFormState, useFormStatus } from 'react-dom';
+import { deleteListing } from '../actions';
 
 type Props = {
   listingId: string;
 };
 
-const DeleteListingButton = ({ listingId }: Props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-
-  const handleCancelReservation = async () => {
-    try {
-      setIsLoading(true);
-
-      const res = await fetch(`/api/listings/${listingId}`, {
-        method: 'DELETE',
-      });
-      const data = await res.json();
-
-      if (!res.ok) {
-        setIsLoading(false);
-        throw Error(data.message);
-      }
-      toast.success('Delete listing successfully');
-      router.refresh();
-    } catch (error) {
-      toast(
-        error instanceof Error ? error.message : 'Failed to cancel reservation'
-      );
-    } finally {
-      setIsLoading(false);
-    }
-  };
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
   return (
-    <Button disabled={isLoading} onClick={handleCancelReservation}>
-      Delete
+    <Button disabled={pending} type="submit">
+      {pending ? 'Deleting' : 'Delete'}
     </Button>
+  );
+};
+
+const DeleteListingButton = ({ listingId }: Props) => {
+  const deleteListingWithId = deleteListing.bind(null, listingId);
+  const [state, dispatch] = useFormState(deleteListingWithId, {
+    message: '',
+  });
+
+  return (
+    <form action={dispatch}>
+      <SubmitButton />
+    </form>
   );
 };
 
